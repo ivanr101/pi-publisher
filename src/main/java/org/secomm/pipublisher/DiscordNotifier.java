45e5b9ec17fb2e4ada7fa5bd96509adaf66afb42
Steve Brenneis <swbrenneis@gmail.com>
2021-08-05 22:47:50 -0400

    Initial add.

---
+++ src/main/java/org/secomm/pipublisher/DiscordNotifier.java
@@ -0,0 +1,68 @@
+package org.secomm.pipublisher;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.secomm.pipublisher.webhook.Embed;
+import org.secomm.pipublisher.webhook.WebhookContent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Scope("prototype")
+@PropertySource("classpath:content.properties")
+public class DiscordNotifier {
+
+    private static final Logger log = LoggerFactory.getLogger(DiscordNotifier.class);
+
+    @Value("${webhook.username}")
+    private String username;
+
+    @Value("${webhook.avatar_url}")
+    private String avatarUrl;
+
+    private final Gson gson;
+
+    private String webHook;
+
+    public DiscordNotifier() {
+
+        gson = new GsonBuilder().create();
+    }
+
+    public void sendWebhook(String title, String content, List<Embed> embeds) {
+
+        WebhookContent webhookContent = new WebhookContent(username, avatarUrl, "", new ArrayList<>());
+        try {
+            webhookContent.setContent(content);
+            webhookContent.setEmbeds(embeds);
+            String json = gson.toJson(webhookContent);
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(webHook))
+                    .POST(HttpRequest.BodyPublishers.ofString(json))
+                    .header("Content-Type", "application/json")
+                    .build();
+
+            HttpClient client = HttpClient.newBuilder().build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            log.info("Discord webhook notification response: {}", response.body());
+        } catch (Exception e) {
+            log.error("Exception in discord webhook notifier: {}", e.getLocalizedMessage());
+        }
+
+    }
+
+    public void setWebHook(String webHook) {
+        this.webHook = webHook;
+    }
+}
