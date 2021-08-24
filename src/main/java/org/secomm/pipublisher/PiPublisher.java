4fb115e42d118d18ec36c1f22c685b537b449507
Steve Brenneis <swbrenneis@gmail.com>
2021-08-24 11:36:52 -0400

    Handover commit

--- src/main/java/org/secomm/pipublisher/PiPublisher.java
+++ src/main/java/org/secomm/pipublisher/PiPublisher.java
@@ -1,15 +1,27 @@
 package org.secomm.pipublisher;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:bots.properties")
 public class PiPublisher implements CommandLineRunner {
+
+    @Value("${token}")
+    private String token;
 
     @Autowired
     private Publisher publisher;
+
+    @Autowired
+    private Editor editor;
 
     public static void main(String... args) {
         SpringApplication.run(PiPublisher.class);
@@ -17,6 +29,12 @@ public class PiPublisher implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        publisher.publish();
+
+        JDA jda = JDABuilder.createLight(token, GatewayIntent.GUILD_MESSAGES)
+                .build();
+        jda.awaitReady();
+
+//        publisher.publish(jda);
+        editor.edit(jda);
     }
 }
--- src/main/java/org/secomm/pipublisher/Publisher.java
+++ src/main/java/org/secomm/pipublisher/Publisher.java
@@ -1,18 +1,33 @@
 package org.secomm.pipublisher;
 
-import org.secomm.pipublisher.webhook.Embed;
-import org.secomm.pipublisher.webhook.Field;
-import org.secomm.pipublisher.webhook.Image;
-import org.secomm.pipublisher.webhook.WebhookContent;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.TextChannel;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class Publisher {
-    public void publish() {
+
+    private static final String SOURCE_CHANNEL = "";
+
+    private static final String DESTINATION_CHANNEL = "865011235678584852";
+
+    private static final String MESSAGE_TO_COPY = "";
+
+    public void publish(JDA jda) {
+
+        TextChannel channel = jda.getTextChannelById(DESTINATION_CHANNEL);
+        if (channel != null) {
+            MessageBuilder messageBuilder = new MessageBuilder();
+            EmbedBuilder embedBuilder = new EmbedBuilder();
+            embedBuilder.setDescription("This is a test message with an embed");
+            embedBuilder.addField(new MessageEmbed.Field("A field",
+                    "A multiline field\n\nTo test appending content", false));
+            messageBuilder.setEmbeds(embedBuilder.build());
+            channel.sendMessage(messageBuilder.build()).queue();
+        }
     }
+
 }
